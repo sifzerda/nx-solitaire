3cdc6adf34642e1c38c550cb1a0541119f35a162
Troygit config --global user.email <tydamon@hotmail.com>
2026-03-29 16:02:53 +1100

    v

--- app/login/page.js
+++ app/login/page.js
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState } from "react";
-import { signIn } from "next-auth/react";
+import { useRouter } from 'next/navigation';
 import { useAuth } from '../../lib/authContext';
 
 export default function LoginPage() {
